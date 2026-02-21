46c3fdbea0958ee1c3d5e9941e0361ded30342ca
Francesc Vendrell <xescoo@gmail.com>
2026-02-21 23:00:01 +0100

    Store Terraform state in GCS with locking and versioning

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.5"
 
+  backend "gcs" {
+    bucket = "moonpay-devops-challenge-tfstate"
+    prefix = "terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
